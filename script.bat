678031f927b0fdb58b7d4949449842fe0adca87e
hamael05 <126512514+hamael05@users.noreply.github.com>
2024-05-10 09:09:44 +0300

    test commit

--- script.bat
+++ script.bat
@@ -3,9 +3,9 @@ set "jdk_path=C:\Program Files\Java\jdk-17"
 mkdir /s /q tempjava
 mkdir javacompiler
 mkdir tempjava
-for /r src %%f in (*.java) do {
+for /r src %%f in (*.java) do (
     copy /y "%%f" tempjava\
-}
+)
 
 "%jdk_path%\bin\javac" -d javacompiler tempjava\*.java
 "%jdk_path%\bin\jar" cf framework-ETU002500.jar -C javacompiler\ .
---
+++ src/mg/ituProm16/FrontController.java
@@ -0,0 +1,28 @@
+package mg.ituProm16;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+    public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("URL: " + req.getRequestURL());
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.processRequest(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.processRequest(req, resp);
+    }
+
+    
+}
