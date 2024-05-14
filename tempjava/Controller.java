8c53c9e6027451ce4ea869117587c48a05ef4a3e
hamael05 <126512514+hamael05@users.noreply.github.com>
2024-05-14 14:09:07 +0300

    commit1

---
+++ tempjava/Controller.java
@@ -0,0 +1,8 @@
+package mg.ituProm16.annotaion;
+
+import java.lang.annotaion.Retention;
+import java.lang.annotaion.RetentionPolicy;
+
+@Rentention(RetentionPolicy.RUNTIME)
+ public @interface Controller {}
+
