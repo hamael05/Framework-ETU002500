7ac9e24dfc3317cb3a9429450290a9846e06be3b
hamael05 <126512514+hamael05@users.noreply.github.com>
2024-05-24 20:57:48 +0300

    c1

---
+++ src/mg/ituProm16/utilitaire/Utilitaire.java
@@ -0,0 +1,30 @@
+package mg.ituProm16.utilitaire;
+
+import java.util.HashMap;
+import java.util.List;
+import java.lang.reflect.Method;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+
+import mg.ituProm16.annotation.*;
+
+public class Utilitaire {
+    public static void scan (Class clazz, HashMap<String, Mapping> hashMap){
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Get.class)){
+                Get get = method.getAnnotation(Get.class);
+                hashMap.put(get.value(), new Mapping(clazz.getName(), method.getName()));
+            }
+        }
+    }
+
+    public static void scanAllClasses (List<Class> classes, HashMap<String, Mapping> hashMap) {
+        for (Class clazz : classes) {
+            scan(clazz, hashMap);
+        }
+    }
+}
