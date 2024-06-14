6811f5cdec1d2d88d23a4c187adeae9bc9b79b71
hamael05 <126512514+hamael05@users.noreply.github.com>
2024-06-14 08:17:40 +0300

    Sprint 5
    Miala tsiny fa namerina any ity sprint 5 ty za satria tsy voa commit ilay izy taminy farany alohany merge de lasa sprint4 ihany no vo merge tany au lieu de sprint 5

---
+++ tempjava/ModelView.java
@@ -0,0 +1,41 @@
+package mg.ituProm16.utilitaire;
+
+import java.util.*;
+
+
+public class ModelView {
+    String url;
+    HashMap<String,Object> data;
+
+    public ModelView(String url, HashMap<String,Object> data){
+        this.url = url;
+        this.data = data;
+    }
+
+    public ModelView(String url){
+        this.data = new HashMap<String,Object>();
+        this.url = url;
+    }
+
+    public ModelView(){}
+
+    public String getUrl(){
+        return this.url;
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public HashMap<String, Object> getData(){
+        return this.data;
+    }
+
+    public void setData(HashMap<String,Object> data){
+        this.data = data;
+    }
+
+    public void add(String key, Object value){
+        data.put(key, value);
+    }
+}
